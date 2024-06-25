807c0366196b6534b1efcc8cff99f3188a37c94b
syafiqiiman98 <syafiqiiman98@gmail.com>
2024-06-25 17:49:01 +0800

    updtd

--- LAB/Python/dummies/del.py
+++ LAB/Python/dummies/del.py
@@ -1,3 +1,12 @@
 bs = int(input())
-gs = bs + hra + da
-    
+hra1 = 0.15
+hra2 = 5000
+da1 = 0.9
+da2 = 0.98
+
+if bs < 15000:
+    gs = bs + (hra1*bs) + (da1*bs)
+    print(f"{gs:.2f}")
+else:
+    gs = bs + hra2 + (da2*bs)
+    print(f"{gs:.2f}")
