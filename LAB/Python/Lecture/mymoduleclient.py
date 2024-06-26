83b50a76257c480a2575c07c597a36d1a6fa37ca
syafiqiiman98 <syafiqiiman98@gmail.com>
2024-06-26 15:10:53 +0800

    ml

--- LAB/Python/Lecture/mymoduleclient.py
+++ LAB/Python/Lecture/mymoduleclient.py
@@ -1,4 +1,12 @@
-import mymodule
+#First method
+# import mymodule
+# print("Interest:", mymodule.simpleInterest(1000,1,6))
 
-print("Interest:", mymodule.simpleInterest(1000,1,6))
+#second method:
+# from mymodule import simpleInterest
+# print("Interest:",simpleInterest(1000,1,6))
 
+#third method
+# from mymodule import*
+# from mymodule import simpleInterest
+# print("Interest:",simpleInterest(1000,1,6)
