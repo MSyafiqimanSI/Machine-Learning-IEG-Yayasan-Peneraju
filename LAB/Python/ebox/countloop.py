ac86c86d3285e17536d2b983cece3c9bbf90b9fa
syafiqiiman98 <syafiqiiman98@gmail.com>
2024-06-26 10:15:56 +0800

    new

---
+++ LAB/Python/ebox/countloop.py
@@ -0,0 +1,33 @@
+a = int(input())
+
+even = 0
+odd = 0
+
+for i in range(a):
+    x =  int(input())
+    if x % 2 == 0:
+        even += 1
+    else:
+     odd += 1
+
+print(even,odd)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     evenNumber = []
+#     oddNumber = []
+#     if child % 2 == 0:
+#         evenNumber.append(child)
+    
+#     else:
+#         oddNumber.append(child)
+
+# print(evenNumber)
+# print(oddNumber)
