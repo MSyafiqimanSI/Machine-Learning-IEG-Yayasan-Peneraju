7bda62bd95aa35c5e105399fa8b7cf092b700cd6
syafiqiiman98 <syafiqiiman98@gmail.com>
2024-06-28 00:18:54 +0800

    kkkk

--- LAB/Python/dummies/del.py
+++ LAB/Python/dummies/del.py
@@ -1,12 +1,7 @@
-bs = int(input())
-hra1 = 0.15
-hra2 = 5000
-da1 = 0.9
-da2 = 0.98
+def darab (n):
+    for i in range(1,13):
+        print(i,"x", n, "=", i*n)
 
-if bs < 15000:
-    gs = bs + (hra1*bs) + (da1*bs)
-    print(f"{gs:.2f}")
-else:
-    gs = bs + hra2 + (da2*bs)
-    print(f"{gs:.2f}")
+
+n = int(input())
+darab(n)
---
+++ LAB/Python/gcolabexercise/funcmulti.py
@@ -0,0 +1,8 @@
+def mul (a, b):
+    multiplication = a * b
+    return multiplication
+
+
+a = int(input())
+b = int(input())
+print (mul(a,b))
