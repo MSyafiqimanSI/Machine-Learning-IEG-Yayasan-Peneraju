ed598f5a5c6febc35ab9742e3cf642eb2184e905
syafiqiiman98 <syafiqiiman98@gmail.com>
2024-07-04 00:42:28 +0800

    third commit

--- LAB/Python/dummies/dummy69.py
+++ LAB/Python/dummies/dummy69.py
@@ -1,1 +1,32 @@
-add
+is_valid = True
+count = 0
+count_a = 0
+
+# for x in range(1,100):
+#         print(x)
+
+# while count_a != 31:
+#      print(count_a)
+#      count_a += 1
+#     #  count +=1
+     
+#      if count_a == 31:
+#             is_valid = False
+#             print("selesai")
+#             break
+
+     
+
+# # else:
+# #     print("xxxx")
+#     is_valid = False
+
+while is_valid:
+    print(count)
+    count += 1
+
+    if count == 50:
+         is_valid = False
+         print(is_valid)
+         break
+
